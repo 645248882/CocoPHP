e3366610062d8cf7b341316b9590feb71510ec44
sunli <382761857@qq.com>
2014-06-10 18:01:16 +0800

    test redis case

--- Project/Controller/Index.php
+++ Project/Controller/Index.php
@@ -1,11 +1,13 @@
 <?php
 class Controller_Index extends Core_Controller_Abstract {
+
+
+    
     public function indexAction()
     {
         $redis = Com_Cache::factory('redis');
         $redis->set('foo', 'Hello world'); 
         echo $redis->get('foo'); 
-    	return false;
     }
 
     public function testAction()
