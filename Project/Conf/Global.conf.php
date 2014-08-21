ca66e820e0e016b4a9fd4361d88f00a89fd11f36
sunli <382761857@qq.com>
2014-08-22 07:43:33 +0800

    change Global Config

--- Project/Conf/Global.conf.php
+++ Project/Conf/Global.conf.php
@@ -6,5 +6,8 @@ define('DEBUG_MODE', true);
 // 调试模式下，是否 Explain SQL
 define('DEBUG_EXPLAIN_SQL', true);
 
+// 模板文件扩展名
+define('TPL_EXT', '.phtml');
+
 // 当前时区
 define('CUR_TIMEZONE', 'Asia/Shanghai');
