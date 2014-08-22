710f0d3ae40dad3cb16b1ab88d17a6fca6705515
sunli <382761857@qq.com>
2014-08-22 10:07:58 +0800

    core

--- Project/Conf/Global.conf.php
+++ Project/Conf/Global.conf.php
@@ -9,14 +9,17 @@ define('DEBUG_EXPLAIN_SQL', true);
 // 模板文件扩展名
 define('TPL_EXT', '.phtml');
 
+// 配置文件目录
+define('CONF_PATH',  APP_PATH  . 'Conf' . DS);
+
 // 数据存放目录
-define('DATA_PATH',     APP_PATH  . 'Data'     . DS);
+define('DATA_PATH',  APP_PATH  . 'Data' . DS);
 
 // 日志目录
-define('LOG_PATH',      DATA_PATH . 'Logs'     . DS);
+define('LOG_PATH',  DATA_PATH . 'Logs' . DS);
 
 // 文件缓存目录
-define('CACHE_PATH',    DATA_PATH . 'Cache'    . DS);
+define('CACHE_PATH', DATA_PATH . 'Cache' . DS);
 
 // 当前时区
 define('CUR_TIMEZONE', 'Asia/Shanghai');
